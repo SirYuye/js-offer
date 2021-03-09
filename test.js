5f2fc51dee2c6b04138ad57c5f514811fef926fc
yuhaihe <121786502@qq.com>
2021-03-09 23:54:43 +0800

    栈

--- test.js
+++ test.js
@@ -1,28 +1,13 @@
-/**
- * @param {number[]} nums
- */
-var NumArray = function(nums) {
-    this.array = nums;
-};
-
-/** 
- * @param {number} i 
- * @param {number} j
- * @return {number}
- */
-NumArray.prototype.sumRange = function(i, j) {
-    let sum = 0;
-    for (let index = i; index <= j; index++) {
-        sum+=this.array[index];
+var removeDuplicates = function(S) {
+  let arr = new Array();
+ for (let i = 0; i < S.length; i++) {
+    if(arr.length > 0 && arr[arr.length-1] === S[i]) {
+        arr.pop();
+    } else {
+        arr.push(S[i]);
     }
-    return sum;
+ }
+ return arr.join('')
 };
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * var obj = new NumArray(nums)
- * var param_1 = obj.sumRange(i,j)
- */
-var obj = new NumArray([-2, 0, 3, -5, 2, -1])
-var param_1 = obj.sumRange(0,2)
-console.log(param_1)
+console.log(removeDuplicates('abbaca'))
